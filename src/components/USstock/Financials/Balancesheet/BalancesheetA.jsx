cbc8a5b94cf96d73ee1e5549da6d39fb4a885fe1
Nonthawat Laonan <nonlaonun@gmail.com>
2024-11-17 05:45:24 +0700

    Update BalancesheetA.jsx

--- src/components/USstock/Financials/Balancesheet/BalancesheetA.jsx
+++ src/components/USstock/Financials/Balancesheet/BalancesheetA.jsx
@@ -1,6 +1,5 @@
 import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
-import './Balancesheet.css';
 
 function BalancesheetA() {
   const [data, setData] = useState([]);
